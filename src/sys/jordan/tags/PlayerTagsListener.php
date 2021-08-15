39b8c242ab0bfe3d54c7d8c71e1cb6a0924789a7
Matthew Jordan <mattjordanbusiness@gmail.com>
2021-08-14 20:21:11 -0500

    PlayerTagsListener: Simplify logic & add priorities to event handlers

--- src/sys/jordan/tags/PlayerTagsListener.php
+++ src/sys/jordan/tags/PlayerTagsListener.php
@@ -6,19 +6,16 @@ namespace sys\jordan\tags;
 
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
-use pocketmine\network\mcpe\protocol\LoginPacket;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\UUID;
-use sys\jordan\tags\session\PlayerSession;
+use pocketmine\network\mcpe\protocol\types\inventory\UseItemOnEntityTransactionData;
+use pocketmine\player\Player;
 use sys\jordan\tags\utils\PlayerTagsBaseTrait;
 
 class PlayerTagsListener implements Listener {
-
 	use PlayerTagsBaseTrait;
 
 	/**
@@ -28,52 +25,43 @@ class PlayerTagsListener implements Listener {
 	public function __construct(PlayerTagsBase $plugin) {
 		$this->setPlugin($plugin);
 		$plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
-		$plugin->getLogger()->info(TextFormat::GREEN . "Successfully enabled listener!");
+		$plugin->getLogger()->debug("Successfully enabled listener!");
 	}
 
-	/**
-	 * @param PlayerPreLoginEvent $event
-	 *
-	 * Create new player session
-	 */
-	public function handlePlayerPreLogin(PlayerPreLoginEvent $event): void {
+	public function handlePlayerJoin(PlayerJoinEvent $event): void {
 		$player = $event->getPlayer();
-		$session = $this->getPlugin()->getSessionManager()->find($player);
-		if($session instanceof PlayerSession) {
-			$session->setPlayer($event->getPlayer());
-			$session->start($this->getPlugin());
-		}
+		$this->getPlugin()->getLogger()->debug("Starting player session for {$player->getName()}...");
+		$session = $this->getPlugin()->getSessionManager()->get($player);
+		$session->start($this->getPlugin());
 	}
 
 	/**
 	 * @param PlayerQuitEvent $event
 	 *
-	 * Destroy player session
+	 * @priority LOWEST
 	 */
 	public function handleQuit(PlayerQuitEvent $event): void {
-		$this->getPlugin()->getSessionManager()->remove($event->getPlayer());
-		$this->getPlugin()->getLogger()->debug("Destroying player session");
+		$player = $event->getPlayer();
+		$this->getPlugin()->getSessionManager()->remove($player);
+		$this->getPlugin()->getLogger()->debug("Destroying player session for {$player->getName()}...");
 	}
 
 	/**
 	 * @param DataPacketReceiveEvent $event
+	 *
+	 * @priority LOWEST
 	 */
 	public function handleDataPacketReceive(DataPacketReceiveEvent $event): void {
-		$player = $event->getPlayer();
 		$packet = $event->getPacket();
-		if($packet instanceof LoginPacket) {
-			$session = $this->getPlugin()->getSessionManager()->create(UUID::fromString($packet->clientUUID));
-			$session->setDevice($packet->clientData["DeviceModel"]);
-			$session->setInputMode($packet->clientData["CurrentInputMode"]);
-			$session->setOS($packet->clientData["DeviceOS"]);
-			$this->getPlugin()->getLogger()->debug("Creating player session");
-		} elseif($packet instanceof InventoryTransactionPacket) {
-			if($packet->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY) {
-				$this->getPlugin()->getSessionManager()->find($player)->addClick();
-			}
-		} elseif($packet instanceof LevelSoundEventPacket) {
-			if($packet->sound === LevelSoundEventPacket::SOUND_ATTACK_NODAMAGE) {
-				$this->getPlugin()->getSessionManager()->find($player)->addClick();
+		$player = $event->getOrigin()->getPlayer();
+		if($player instanceof Player && $player->isOnline()) {
+			$session = $this->getPlugin()->getSessionManager()->get($player);
+			if($packet instanceof InventoryTransactionPacket && $packet->trData instanceof UseItemOnEntityTransactionData) {
+				$session->addClick();
+			} elseif($packet instanceof LevelSoundEventPacket) {
+				if($packet->sound === LevelSoundEventPacket::SOUND_ATTACK_NODAMAGE) {
+					$session->addClick();
+				}
 			}
 		}
 	}
