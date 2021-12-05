47e4f3172ee7e96e340c6069ac643fbc52904897
Em Jordan <sylvrs@proton.me>
2021-12-05 16:03:06 -0600

    Implement migration checker for old tags from 3.0 -> 4.0

--- src/sys/jordan/tags/tag/TagFactory.php
+++ src/sys/jordan/tags/tag/TagFactory.php
@@ -20,6 +20,7 @@ use sys\jordan\tags\tag\group\external\RankUpTagGroup;
 use sys\jordan\tags\tag\group\external\RedSkyBlockTagGroup;
 use sys\jordan\tags\tag\group\external\SkyBlockTagGroup;
 use sys\jordan\tags\tag\group\TagGroup;
+use sys\jordan\tags\tag\migration\MigrationChecker;
 use sys\jordan\tags\utils\PlayerTagsBaseTrait;
 use pocketmine\utils\TextFormat;
 
@@ -46,6 +47,22 @@ class TagFactory {
 	public function __construct(PlayerTagsBase $plugin) {
 		$this->setPlugin($plugin);
 		$this->tag = $plugin->getConfig()->get("tag", "");
+		if(strlen($this->tag) > 0) {
+			$result = MigrationChecker::checkTag($this->tag);
+			$plugin->getLogger()->debug("Checking global tag for outdated tag types...");
+			if($result->hasChanged()) {
+				foreach($result->changed as $old => [$new, $count]) {
+					$this->getPlugin()->getLogger()->debug("Located $count tag(s) of type $old. Converting to type $new...");
+				}
+				$this->getPlugin()->getLogger()->debug("Migrating outdated tags to new types...");
+				$this->tag = $result->newTag;
+				// Save changes to disk
+				$plugin->getConfig()->set("tag", $this->tag);
+				$plugin->getConfig()->save();
+				$this->getPlugin()->getLogger()->info(TextFormat::YELLOW . "Outdated tags were found in the global tag and have automatically been converted to the new tag types.");
+			}
+		}
+
 		$this->colorCharacter = $plugin->getConfig()->get("color-character", "&");
 		$this->updatePeriod = $plugin->getConfig()->get("update-period", 10);
 		$this->tagManager = new MultiWorldTagManager($this, $this->getPlugin()->getConfig());
